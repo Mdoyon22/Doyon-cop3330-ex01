0c4c1ff94dcfb6247f954f86e77a6443d9fa6e5c
Michael <michaeldoyon1@gmail.com>
2021-08-31 16:44:57 -0400

    Initial Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+import java.util.Scanner;
+
+/**
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Michael Doyon
+ */
+
+
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.print("What is your name? ");
+        Scanner scan = new Scanner(System.in);
+        String name = scan.next();
+        System.out.println("Hello, " + name + ", nice to meet you! " );
+    }
+}
